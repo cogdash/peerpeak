b22389acb935470f1102f06996c488fc86c7a6a4
Reeliks <reeeeeliks@gmail.com>
2026-07-30 05:21:15 +0300

    Added Jinja2, a template folder and a funny home page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+import os
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
-@app.get("/")
-def read_root():
-    return "PeerPeak is ready!"
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse(request, "index.html", context)
 
 
-@app.get("/")
+@app.get("/achievements")
 def get_achievements():
     return "Bikepacking"
